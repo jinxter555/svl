d7d16d56a6efa2d5c19f1e295c7241cbf55d7f5f
jt <jt@jt-Dell-G15-5515.mynetworksettings.com>
2023-07-14 23:46:20 -0400

    9th commit

--- vm_svl.cpp
+++ vm_svl.cpp
@@ -1,3 +1,4 @@
+#include "expr.hh"
 #include "token.hh"
 #include "vm_svl.hh"
 
@@ -5,3 +6,29 @@ using namespace std;
 void VMsvl::push(Token tv) {
   stack.push(tv);
 }
+Token VMsvl::pop() {
+  Token v = stack.top();
+  stack.pop();
+  return v;
+}
+
+void VMsvl::call(int address) {
+  frames.push_back(Frame(pc, fp));
+  fp = stack.size();
+  pc = address;
+}
+
+void VMsvl::decl(const string& name) {
+  context.locals[name] = vp;
+  vp++;
+}
+
+void VMsvl::add_expr(Expr& expr) { // takes variable expr modify th
+    auto ep = expr.ToPostfix();
+    code.insert(code.end(), ep.begin(), ep.end());
+}
+
+void VMsvl::run() {
+}
+
+
--- vm_svl.hh
+++ vm_svl.hh
@@ -7,39 +7,45 @@
 #include "token.hh"
 #include "readinputline.hh"
 
+#pragma once
+
 using namespace std;
 
-struct InterpreterContext {
-    // Current instruction pointer
-    int ip;
-    // Current stack pointer
-    int sp;
-    int fp;
+class Frame {
+public:
+    int pc=0; // Current instruction pointer
+    int sp=0; // Current stack pointer, current size is the current index
+    int vp=0; // variable pointer stack[fp+vp]
+    int fp=0;
     // Current heap size
-    int heap_size;
+    int heap_size=0;
     // Garbage collection status
-    bool gc_enabled;
+    bool gc_enabled=false;
 
     // Current stack frame
     //StackFrame* current_frame;
     // Local variables
-    std::unordered_map<std::string, any> locals;
+    std::unordered_map<std::string, int> locals;    // int is from fp + int
+
+    Frame(int pc, int fp) : pc(pc), fp(fp) {}
 };
 
 class VMsvl {
 private:
   vector<Token> code; // postfix expressions
-  stack<Token> stack; // postfix expressions
+  stack<Token> stack; // tokens stack
+  vector<Frame> frames;
+  Frame context;
+  int pc = 0;         // program counter
+  int fp = 0;         // frame counter
+
 public:
-  void add_expr(Expr& expr) { // takes variable expr modify th
-    auto ep = expr.ToPostfix();
-    code.insert(code.end(), ep.begin(), ep.end());
-
-  }
-
-
   void run();
+  void add_expr(Expr& expr);
+
   void push(Token v); 
+  void call(int address);
+  void decl(const string& name);    // declare local variable
   Token pop();
 
 
@@ -47,7 +53,7 @@ public:
 
 int main() {
   ReadInputLine ril;
-  vm_svl vm;
+  VMsvl vm;
   ril.tokenize("1 + 2");
   Expr e(ril);
   vm.add_expr(e);
