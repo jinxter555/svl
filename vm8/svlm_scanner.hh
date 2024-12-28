ea771fefb77a85b016ba3df9838d15ef5723f9c6
Jing Tan <jinxter555@gmail.com>
2024-12-28 02:30:10 -0500

    redoing astnode with qnode attribute

--- vm8/svlm_scanner.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-#undef yyFlexLexer
-#define yyFlexLexer SvlmFlexLexer
-#include <FlexLexer.h>
-#include "svlm_scanner_internal.hh"
--- vm8/test_cc/t_ast_01.cc
+++
@@ -1,6 +0,0 @@
-#include "../svlm_ast.hh"
-
-
-int main() {
-  auto a = make_unique<AstBinOp>(make_unique<Operand>(123l), make_unique<Operand>(456l), AstOpCode::plus); 
-}
